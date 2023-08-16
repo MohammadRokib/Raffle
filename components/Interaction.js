ce5b0390294b2df866bb2c943573dbc479c0a34f
MohammadRokib <rokibk548@gmail.com>
2023-08-16 07:05:28 +0600

    Raffle frontend

---
+++ components/Interaction.js
@@ -0,0 +1,8 @@
+import { useWeb3Contract } from 'react-moralis'
+import { abi, contractAddresses } from '../constants/'
+import { useMoralis } from 'react-moralis';
+import { useEffect, useState } from 'react';
+import { ethers } from "ethers";
+import { useNotification } from 'web3uikit';
+
+
